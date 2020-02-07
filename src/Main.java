51cc342367eb2320c9ea74ea51106fb1dc7d923d
lbut <luca.but@isisbem.it>
2020-02-07 12:49:58 +0100

    Centro di amministrazione

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
-        //sas
+        int scelta;
+        System.out.println("Benvenuto in MikiBank! La banca più trasparente dell'acqua!");
+        System.out.println("    Centro di Amministrazione    ");
+        System.out.println("0 ~ Uscita e pulizia della memoria");
+        System.out.println("1 ~ Visualizzazione informazioni clienti");
+        System.out.println("2 ~ Apertura di un conto");
+        System.out.println("3 ~ Gestione di un conto");
+        System.out.println("4 ~ Chiusura di un conto");
+        System.out.println("5 ~ ");
     }
 }
