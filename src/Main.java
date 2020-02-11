c6e75bc87ed0aabbd6e421c4ba718e25da2a29e9
lbut <luca.but@isisbem.it>
2020-02-11 11:36:04 +0100

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,61 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        int scelta;
-        System.out.println("Benvenuto in MikiBank! La banca più trasparente dell'acqua!");
-        System.out.println("    Centro di Amministrazione    ");
-        System.out.println("0 ~ Uscita e pulizia della memoria");
-        System.out.println("1 ~ Visualizzazione informazioni clienti");
-        System.out.println("2 ~ Apertura di un conto");
-        System.out.println("3 ~ Gestione di un conto");
-        System.out.println("4 ~ Chiusura di un conto");
-        System.out.println("5 ~ ");
+        String scelta;
+        Scanner input = new Scanner(System.in);
+        datiCorrentista correntista = new datiCorrentista();
+        System.out.println("Benvenuto in MikiBank (Registered to Intesa San Pado)! La banca più trasparente dell'acqua!");
+
+        do {
+            System.out.println("    Centro di Amministrazione    ");
+            System.out.println("Scelga una voce dal seguente menù");
+            System.out.println("0 ~ Uscita e pulizia della memoria");
+            System.out.println("1 ~ Visualizzazione Scheda Identificativa Cliente");
+            System.out.println("2 ~ Registrazione Scheda Identificativa Cliente");
+            System.out.println("3 ~ Apertura di un conto");
+            System.out.println("4 ~ Gestione di un conto");
+            System.out.println("5 ~ Chiusura di un conto");
+            scelta = input.next();
+            switch (scelta) {
+                case "0":
+                    System.exit(0);
+                    System.gc();
+                    break;
+
+                case "1":
+                    //Visualizzazione SIC
+                    System.out.println("    Scheda Identificativa Cliente   ");
+                    break;
+
+                case "2":
+                    //Registrazione SIC
+                    System.out.println("    Inizializzazione Scheda Identificativa Cliente  ");
+                    System.out.println("L'individuo è un maschio o una femmina?");
+                    correntista.setSesso(input.nextBoolean());
+                    System.out.println("Cognome dell'individuo: ");
+                    correntista.setCognome(input.next());
+                    System.out.println("Nome dell'individuo: ");
+                    correntista.setNome(input.next());
+                    break;
+
+                case "3":
+                    //Apertura conto
+                    break;
+
+                case "4":
+                    //Gestione conto
+                    break;
+
+                case "5":
+                    //Chiusura conto
+                    break;
+
+
+                default:
+                    System.out.println("La selezione da lei effettuata non corrisponde a nessuna voce sopra indicata! Riprovare per cortesia.");
+                    break;
+            }
+
+        } while(!scelta.equals("0"));
     }
 }
---
+++ src/datiCorrentista.java
@@ -0,0 +1,123 @@
+public class datiCorrentista {
+    String cognome;
+    String nome;
+    String codiceFiscale;
+    String dataNascita;
+    String luogoNascita;
+    String residenza;
+    String cittadinanza;
+    String statoCivile;
+    boolean accreditoStipendio;
+    boolean sesso;
+    boolean clienteRegistrato;
+
+    public datiCorrentista(){}
+
+    public datiCorrentista(String cognome, String nome, String codiceFiscale, String dataNascita, String luogoNascita, String residenza, String cittadinanza, String statoCivile, boolean accreditoStipendio, boolean sesso, boolean clienteRegistrato) {
+        this.cognome = cognome;
+        this.nome = nome;
+        this.codiceFiscale = codiceFiscale;
+        this.dataNascita = dataNascita;
+        this.luogoNascita = luogoNascita;
+        this.residenza = residenza;
+        this.cittadinanza = cittadinanza;
+        this.statoCivile = statoCivile;
+        this.accreditoStipendio = accreditoStipendio;
+        this.sesso = sesso;
+        this.clienteRegistrato = clienteRegistrato;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    public String getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(String dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    public String getLuogoNascita() {
+        return luogoNascita;
+    }
+
+    public void setLuogoNascita(String luogoNascita) {
+        this.luogoNascita = luogoNascita;
+    }
+
+    public String getResidenza() {
+        return residenza;
+    }
+
+    public void setResidenza(String residenza) {
+        this.residenza = residenza;
+    }
+
+    public String getCittadinanza() {
+        return cittadinanza;
+    }
+
+    public void setCittadinanza(String cittadinanza) {
+        this.cittadinanza = cittadinanza;
+    }
+
+    public String getStatoCivile() {
+        return statoCivile;
+    }
+
+    public void setStatoCivile(String statoCivile) {
+        this.statoCivile = statoCivile;
+    }
+
+    public boolean isAccreditoStipendio() {
+        return accreditoStipendio;
+    }
+
+    public void setAccreditoStipendio(boolean accreditoStipendio) {
+        this.accreditoStipendio = accreditoStipendio;
+    }
+
+    public boolean isSesso() {
+        return sesso;
+    }
+
+    public void setSesso(boolean sesso) {
+        this.sesso = sesso;
+    }
+
+    public boolean isClienteRegistrato() {
+        return clienteRegistrato;
+    }
+
+    public void setClienteRegistrato(boolean clienteRegistrato) {
+        this.clienteRegistrato = clienteRegistrato;
+    }
+
+    @Override
+    public String toString() {
+        return "Le informazioni sull'individuo correntista sono:"
+                + "";
+    }
+}
